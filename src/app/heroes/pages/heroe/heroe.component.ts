8a9e31adc896375d477479bb0bdc1ef71426c6ef
Jannety Gutierrez <netygtz@gmail.com>
2021-11-23 10:28:20 -0700

    app hasta la segunda sección

---
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styles: [`
+  img{
+    width:100%;
+    border-radius: 5px;
+  }`
+  ]
+})
+export class HeroeComponent implements OnInit {
+
+  heroe!: Heroe;
+
+  constructor(private activatedRoute: ActivatedRoute, 
+              private heroesService: HeroesService,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    // this.activatedRoute.params.subscribe(({ id }) => console.log(id));
+
+
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.heroesService.getHeroePorId(id)),
+        tap(console.log)
+      ).subscribe(heroe => {
+        this.heroe = heroe;
+        console.log(this.heroe);
+      });
+  }
+
+  regresar(){
+    this.router.navigate(['/heroes/listado'])
+  }
+
+}
